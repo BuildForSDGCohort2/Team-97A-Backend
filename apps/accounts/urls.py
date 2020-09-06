226f46ec8a4d9b4f0a7843af0c4a61dec7176d97
abdoulrasheed <abdoulrasheedibrahim47@gmail.com>
2020-09-06 18:03:41 +0100

    Add login ability

--- apps/accounts/urls.py
+++ apps/accounts/urls.py
@@ -1,6 +1,11 @@
 from django.urls import path, include
+from rest_framework_simplejwt import views as jwt_views
 
+app_name = "accounts"
 urlpatterns = [
     path('', include('dj_rest_auth.urls')),
-    path('register', include('dj_rest_auth.registration.urls')),
+    path('register/', include('dj_rest_auth.registration.urls')),
+    path('refresh/', jwt_views.TokenRefreshView.as_view(), name='token_refresh'),
+    path('obtain/', jwt_views.TokenObtainPairView.as_view(),
+         name='token_obtain_pair'),
 ]
--- apps/main/views.py
+++ apps/main/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
 
-# Create your views here.
+
+class DashboardView(APIView):
+    permission_classes = (IsAuthenticated,)
+
+    def get(self, request):
+        content = {'message': 'Hello Dashboad!'}
+        return Response(content)
