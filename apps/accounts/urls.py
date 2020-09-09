8bac5da616eba59ca7996a0e350de05117e10b9a
JeremAIh <esiebomaj@gmail.com>
2020-09-09 15:49:37 +0100

    added generic email confirmation path in urls.py

--- apps/accounts/urls.py
+++ apps/accounts/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path, include
 from rest_framework_simplejwt import views as jwt_views
 
+
 app_name = "accounts"
 urlpatterns = [
     path('', include('dj_rest_auth.urls')),
@@ -8,4 +9,5 @@ urlpatterns = [
     path('refresh/', jwt_views.TokenRefreshView.as_view(), name='token_refresh'),
     path('obtain/', jwt_views.TokenObtainPairView.as_view(),
          name='token_obtain_pair'),
+
 ]
--- carigobackend/urls.py
+++ carigobackend/urls.py
@@ -1,9 +1,14 @@
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
+from django.views.generic import TemplateView
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/v1/', include('main.urls')),
     path('api/v1/accounts/', include('accounts.urls')),
+
+    re_path(r'^account-confirm-email/(?P<key>[-:\w]+)/$', TemplateView.as_view(),
+            name='account_confirm_email'),  # Generic email confirmation path to bypass email cnfirmation for now
 
 ]
